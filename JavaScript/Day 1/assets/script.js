ccb4e806955f388648cbdf38c55861d16a6f5d4c
maheshwaran <mw906782@gmail.com>
2026-09-21 23:42:01 +0530

    update new code

---
+++ JavaScript/Day 1/assets/script.js
@@ -0,0 +1,22 @@
+/*task 1*/
+
+let studentname = "Maheshwaran";
+console.log(studentname)
+
+let Age = 21;
+console.log(Age)
+
+let isstudent = true;
+console.log(isstudent)
+
+let  selecteduser = null;
+console.log(selecteduser)
+
+let city;
+console.log(city)
+
+
+/* task 2 */
+
+let number = 300;
+console.log(number)
